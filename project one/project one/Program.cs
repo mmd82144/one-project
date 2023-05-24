ddd28620fff9bb087807f4dd140467c91209e60c
Mohammad Rezazadeh <mohammad1382144@gmail.com>
2023-05-24 17:06:07 +0330

    name/triple

---
+++ project one/project one/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name");
+            name =Console.ReadLine();
+
+            Console.WriteLine("hello "+ name);
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ two project/two project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace two_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("pls enter a nambr");
+            string number = Console.ReadLine();
+            x = int.Parse(number);
+
+            x *=3;
+            
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
